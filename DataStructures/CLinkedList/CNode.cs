535216279ce3ed4de47611e5840ea7940d843ab3
Nicolas Ferreira <nicolas.l.ferreira@gmail.com>
2021-02-26 17:42:13 +1300

    Moved data structures to the folder

---
+++ DataStructures/CLinkedList/CNode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSDataStructures.CLinkedList
+{
+    class LLNode<T>
+    {
+        public LLNode() { }
+        public LLNode(T data)
+        {
+            Data = data;
+            Point = null;
+        }
+
+        public T Data { get; set; }
+        public LLNode<T> Point { get; set; }
+    }
+}
---
+++ DataStructures/Graph/GNode.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSDataStructures.Graph
+{
+    class GNode<T>
+    {
+        public string Name { get; set; }
+        public T Data { get; set; }
+        public List<GNode<T>> Successors { get; set; }
+        public List<GNode<T>> Predecessors { get; set; }
+
+        public GNode(string name, T data = default)
+        {
+            Data = data;
+
+            Successors = new List<GNode<T>>();
+            Predecessors = new List<GNode<T>>();
+        }
+
+        public void Add(GNode<T> node)
+        {
+            Successors.Add(node);
+            if (!node.Predecessors.Contains(this)) node.Predecessors.Add(this);
+        }
+    }
+}
