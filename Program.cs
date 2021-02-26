e9b7410495222c1c05b2c69f6768832e58052d7b
Nicolas Ferreira <nicolas.l.ferreira@gmail.com>
2021-02-26 17:43:29 +1300

    Moved data structures to inside the folder

--- Program.cs
+++ Program.cs
@@ -4,14 +4,31 @@ using CSDataStructures.CLinkedList;
 using CSDataStructures.HashTable;
 using CSDataStructures.Graph;
 
+using CSDataStructures.Algorithms.Search;
+
 namespace CSDataStructures
 {
     class Program
     {
         static void Main(string[] args)
         {
-            CHashTable<string> h = new CHashTable<string>(20);
-           
+            Search s = new Search();
+
+            int[] arr = new int[24];
+            int number = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                number += i;
+                Console.WriteLine($"{number} : {i}");
+
+                arr[i] = number;
+            }
+
+            Console.WriteLine(number);
+
+            Console.WriteLine($"Jump search: {s.JumpSearch(arr, 78, arr.Length / 6)}");
+            Console.WriteLine($"Binary search: {s.ImprovedLinearSearch(arr, 231)}");
 
             Console.ReadKey();
         }
