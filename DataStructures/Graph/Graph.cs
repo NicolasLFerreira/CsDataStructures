39df96ae38847532ad7fb8685957dbf1234ff8d4
Nicolas Ferreira <nicolas.l.ferreira@gmail.com>
2021-03-05 15:31:40 +1300

    Implemented graph data structure

---
+++ DataStructures/Graph/Graph.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSDataStructures.DataStructures.Graph
+{
+    class Graph<T>
+    {
+        private List<Node<T>> Nodes { get; set; }
+        public int Size { get; set; }
+
+        public Node<T> this[int index]
+        {
+            get
+            {
+                return Nodes[index];
+            }
+            set 
+            {
+                Nodes.Insert(index, value);
+            }
+        }
+
+        public Graph(int size)
+        {
+            Size = size;
+            Nodes = new List<Node<T>>(size);
+        }
+
+        public Graph(List<Node<T>> nodes)
+        {
+            Size = nodes.Count();
+            Nodes = nodes;
+        }
+
+        public void AddVertex(Node<T> node)
+        {
+            Nodes.Add(node);
+        }
+
+        public void RemoveVertex(Node<T> node)
+        {
+            Nodes.Remove(node);
+        }
+
+        public void DepthFirstSearch(Node<T> root)
+        {
+            if (!root.Visited)
+            {
+                Console.WriteLine($"{root.Data} -> ");
+                root.Visit();
+                foreach (Node<T> neighbor in root.Neighbors)
+                {
+                    DepthFirstSearch(neighbor);
+                }
+            }
+        }
+
+        public void BreadthFirstSearch(Node<T> root)
+        {
+            Queue<Node<T>> queue = new Queue<Node<T>>();
+
+            root.Visit();
+
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                Node<T> current = queue.Dequeue();
+
+                foreach (Node<T> neighbor in current.Neighbors)
+                {
+                    Console.WriteLine(neighbor.Data);
+                    neighbor.Visit();
+                    queue.Enqueue(neighbor);
+                }
+            }
+        }
+    }
+}
---
+++ DataStructures/Graph/Node.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSDataStructures.DataStructures.Graph
+{
+    class Node<T>
+    {
+        public T Data { get; set; }
+        public bool Visited { get; private set; }
+        public List<Node<T>> Neighbors { get; set; }
+
+        public Node(T data)
+        {
+            Data = data;
+            Visited = false;
+        }
+
+        public Node(T data, List<Node<T>> neighbors)
+        {
+            Data = data;
+            Neighbors = neighbors;
+            Visited = false;
+        }
+
+        public void AddNeighbor(Node<T> vertex)
+        {
+            Neighbors.Add(vertex);
+        }
+
+        public void AddNeighbor(List<Node<T>> vertices)
+        {
+            Neighbors.AddRange(vertices);
+        }
+
+        public void RemoveNeighbor(Node<T> vertex)
+        {
+            if (Neighbors.Contains(vertex)) Neighbors.Remove(vertex);
+            else throw new Exception();
+        }
+
+        public void Visit()
+        {
+            Visited = true;
+        }
+    }
+}
