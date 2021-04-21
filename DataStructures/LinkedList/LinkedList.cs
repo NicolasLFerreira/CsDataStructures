484d2522b756f59a5455a0bf4704e7789ef7d11a
Nicolas Ferreira <nicolas.l.ferreira@gmail.com>
2021-04-22 10:05:55 +1200

    Changed name

---
+++ DataStructures/LinkedList/LinkedList.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace CSDataStructures.DataStructures.CLinkedList
+{
+    class LinkedList<T>
+    {
+        public LinkedList() { }
+
+        public Node<T> Head { get; private set; }
+        public Node<T> Tail { get; private set; }
+
+        public void Add(T data)
+        {
+            if (Head == null)
+            {
+                Head = new Node<T>(data);
+                Tail = Head;
+            }
+            else
+            {
+                Tail.Point = new Node<T>(data);
+                Tail = Tail.Point;
+            }
+        }
+
+        private Node<T> GetNode(int index)
+        {
+            Node<T> current = Head;
+            for (int i = 0; i < index; i++)
+            {
+                if (current.Point == null) return default;
+                current = current.Point;
+            }
+            return current;
+        }
+
+        public T Get(int index) => GetNode(index).Data;
+
+        public void Delete(int index)
+        {
+            Node<T> current = GetNode(index - 1);
+            current.Point = current.Point.Point;
+        }
+    }
+}
---
+++ DataStructures/LinkedList/Node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CSDataStructures.DataStructures.CLinkedList
+{
+    class Node<T>
+    {
+        public Node() { }
+        public Node(T data)
+        {
+            Data = data;
+            Point = null;
+        }
+
+        public T Data { get; set; }
+        public Node<T> Point { get; set; }
+    }
+}
