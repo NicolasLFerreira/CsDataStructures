87a3febf111580936be52fa7455394d62e9ede93
Nicolas Ferreira <nicolas.l.ferreira@gmail.com>
2021-04-23 11:43:58 +1200

    Implemented tree

--- DataStructures/Tree/BinaryTree.cs
+++ DataStructures/Tree/BinaryTree.cs
@@ -4,5 +4,55 @@ namespace CSDataStructures.DataStructures.Tree
 {
     class BinaryTree
     {
+        public BinaryTree Left { get; set; }
+        public BinaryTree Right { get; set; }
+
+        public int Data { get; set; }
+        
+        public BinaryTree(int data) => Data = data;
+
+        public void Insert(int data)
+        {
+            if (data <= Data)
+            {
+                if (Left == null) Left = new BinaryTree(data);
+                else Left.Insert(data);
+            }
+
+            if (data > Data)
+            {
+                if (Right == null) Right = new BinaryTree(data);
+                else Right.Insert(data);
+            }
+        }
+
+        public bool Contains(int data)
+        {
+            if (data == Data) return true;
+
+            if (Left != null)
+            {
+                if (data == Left.Data) return true;
+                if (data <= Left.Data) Left.Contains(data);
+            }
+
+            if (Right != null)
+            {
+                if (data == Right.Data) return true;
+                if (data > Right.Data) Right.Contains(data);
+            }
+            return false;
+        }
+
+        public void PrintInOrder()
+        {
+            if (Left != null) Left.PrintInOrder();
+            for (int i = 0; i < Data; i++)
+            {
+                Console.Write('.');
+            }
+            Console.WriteLine();
+            if (Right != null) Right.PrintInOrder();
+        }
     }
 }
